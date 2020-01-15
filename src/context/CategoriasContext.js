273be51f0d9512a98a4d33d73c3b6dae4171c50c
Luis Perez Mosteiro <lperez@ntb-158.d01.keplertech.com>
2020-01-15 17:51:48 -0300

    Proyecto de una Aplicacion que muestra los eventos con UIKit

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// Crear el context
+const CategoriasContext = React.createContext();
+
+export const CategoriasConsumer = CategoriasContext.Consumer;
+
+class CategoriasProvider extends Component {
+
+    token = 'RGZSQ3RSRKJUFHU2CXYW';
+
+    state = { 
+        categorias : []
+    }
+
+    componentDidMount() {
+        this.obtenerCategorias();
+    }
+
+    obtenerCategorias = async () => {
+
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+        let categorias = await axios.get(url);
+        // console.log(categorias.data);
+
+        this.setState({
+            categorias : categorias.data.categories
+        })
+
+    }
+
+    render() {
+        return (
+            <CategoriasContext.Provider
+                value= {{
+                    categorias : this.state.categorias
+                }}
+            >
+                {this.props.children}
+            </CategoriasContext.Provider>
+        );
+    }
+}
+
+export default CategoriasProvider;
